e22be349b4f69c24b965b70bb56b2c30f00da823
Sjackson199703 <55985199+Sjackson199703@users.noreply.github.com>
2021-08-02 15:11:43 +0100

    Add files via upload

---
+++ BeeliveryLite/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using BeeliveryLite.BoilerPlate;
+using Xamarin.Forms;
+
+namespace BeeliveryLite
+{
+    public partial class App : Application
+    {
+
+        public Locator Locator { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+            Locator = new Locator(this);
+            _ = Locator.SetFirstPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ BeeliveryLite/BoilerPlate/Locator.cs
@@ -0,0 +1,51 @@
+﻿using FunctionZero.MvvmZero;
+using BeeliveryLite.Pages;
+using BeeliveryLite.PageViewModels;
+using SimpleInjector;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BeeliveryLite.BoilerPlate
+{
+	public class Locator
+	{
+		private Container _IoCC;
+
+		internal Locator(Application currentApplication)
+		{
+			_IoCC = new Container();
+
+			_IoCC.Register<IPageServiceZero>(
+				() =>
+				{
+					var pageService = new PageServiceZero(() => App.Current.MainPage.Navigation, (theType) => _IoCC.GetInstance(theType));
+					return pageService;
+				},
+				Lifestyle.Singleton
+			);
+
+			_IoCC.Register<HomePage>(Lifestyle.Singleton);
+			_IoCC.Register<PlaceOrder>(Lifestyle.Singleton);
+			_IoCC.Register<ViewBasket>(Lifestyle.Singleton);
+			_IoCC.Register<Checkout>(Lifestyle.Singleton);
+			_IoCC.Register<OrderTracking>(Lifestyle.Singleton);
+
+			_IoCC.Register<HomePageVm>(Lifestyle.Singleton);
+			_IoCC.Register<PlaceOrderVm>(Lifestyle.Singleton);
+			_IoCC.Register<ViewBasketVm>(Lifestyle.Singleton);
+			_IoCC.Register<CheckoutVm>(Lifestyle.Singleton);
+			_IoCC.Register<OrderTrackingVm>(Lifestyle.Singleton);
+		}
+		internal async Task SetFirstPage()
+		{
+
+			App.Current.MainPage = new NavigationPage();
+			await _IoCC.GetInstance<IPageServiceZero>().PushPageAsync<HomePage, HomePageVm>((vm) => {});
+		}
+		private void PageCreated(Page thePage)
+		{
+			Debug.WriteLine(thePage);
+		}
+	}
+}
